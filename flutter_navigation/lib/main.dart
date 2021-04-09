2d628e7f9a474cce9f35b46250688bd261da14e2
farihanoor <fariha04noor@gmail.com>
2021-04-09 16:43:43 +0600

    comments added

--- flutter_navigation/lib/main.dart
+++ flutter_navigation/lib/main.dart
@@ -10,10 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //this removes the red label
       debugShowCheckedModeBanner: false,
       title: 'Navigation',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        //AppBar color
+        primarySwatch: Colors.pink,
       ),
       home: _Homepage(),
     );
@@ -29,8 +31,10 @@ class __HomepageState extends State<_Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //the whole screen is white
       backgroundColor: Colors.white,
       appBar: AppBar(
+        //text in the App bar
         title: Text(
           'Home Screen',
           style: TextStyle(
@@ -39,10 +43,15 @@ class __HomepageState extends State<_Homepage> {
           ),
         ),
       ),
+      //a container is set to place the text and button
       body: Container(
         alignment: Alignment.center,
+        //now we set a column ta plact the text and button, on top of the other
         child: Column(
+          //mainAxis the the parallel to y-axis for column
           mainAxisAlignment: MainAxisAlignment.center,
+          //this container will have 2 <Widget>..
+          //i)Text, ii)Padding
           children: <Widget>[
             Text(
               "HomeScreen",
@@ -51,6 +60,7 @@ class __HomepageState extends State<_Homepage> {
                 fontWeight: FontWeight.w600,
               ),
             ),
+            //we define the button here
             Padding(
               padding: EdgeInsets.all(10.0),
               child: RaisedButton(
@@ -58,17 +68,21 @@ class __HomepageState extends State<_Homepage> {
                   horizontal: 30.0,
                   vertical: 15.0,
                 ),
+                //color of the button
                 color: Colors.red,
+                //The main code for navigation starts here
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
                       builder: (
+                      //we point to the constructor of the firstScreen()
                         (context) => firstScreen()
                       ),
                     ),
                   );
                 },
+                //text displayed on the button
                 child: Text(
                   "Screen 1",
                   style: TextStyle(color: Colors.white, fontSize: 25.0),
